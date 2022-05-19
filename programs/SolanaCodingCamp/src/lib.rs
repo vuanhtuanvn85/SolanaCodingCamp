a1b37e0d9d7c8237a41aa3535a924879b9f31a0c
Vu Anh Tuan <vuanhtuanvn85@gmail.com>
2022-05-19 15:03:50 +0700

    add initialize_sum for testing

--- programs/SolanaCodingCamp/src/lib.rs
+++ programs/SolanaCodingCamp/src/lib.rs
@@ -6,10 +6,30 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod solana_coding_camp {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_sum(ctx: Context<InitializeSum>, sum_init: u64) -> Result<()> {
+        let sumAccount = &mut ctx.accounts.sumAccount;
+        sumAccount.sum = sum_init;
         Ok(())
     }
 }
 
+// Khai báo cấu trúc dữ liệu
+#[account]
+pub struct SumAccount {
+    pub sum: u64,
+}
+
+// Khai báo các tài khoản sẽ tương tác
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeSum<'info> {
+    // địa chỉ thuê
+    #[account(init, payer = user, space = 8 + 8 )]
+    pub sumAccount: Account<'info, SumAccount>,
+
+    // người trả phí giao dịch
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    // địa chỉ chương trình giúp thuê tài khoản
+    pub system_program: Program<'info, System>,
+}
