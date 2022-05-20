265491c92474d4aaef4c9b03431fac335d22f0b5
Vu Anh Tuan <vuanhtuanvn85@gmail.com>
2022-05-20 11:08:10 +0700

    add voting programs (candidate, vote, close)

--- programs/SolanaCodingCamp/src/lib.rs
+++ programs/SolanaCodingCamp/src/lib.rs
@@ -2,6 +2,15 @@ use anchor_lang::prelude::*;
 
 declare_id!("4Q71d52Byvx9TcyhDNJXH98HPpvdiAeToyDn1tiqyhcP");
 
+pub mod schema;
+pub use schema::*;
+
+pub mod instructions;
+pub use instructions::*;
+
+pub mod errors;
+pub use errors::*;
+
 #[program]
 pub mod solana_coding_camp {
     use super::*;
